f1544b11eea6b1cb33a647c2a578a0dcf560ac89
喵哩个咪 <wailovet@163.com>
2016-06-14 13:08:06 +0800

    Update wdcp.sh

--- wdcp.sh
+++ wdcp.sh
@@ -1,7 +1,6 @@
-#!bash
+yum install git
 wget http://dl.wdlinux.cn/lanmp_v2.5.tar.gz
 tar zxvf lanmp_v2.5.tar.gz
 sh install.sh
 wget http://www.i5g.me/centos/php_up56.sh
 sh php_up56.sh
-yum install git
